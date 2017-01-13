2c97fc7632516db8d1286b93d48bdc17e45243fa
Nick Momrik <nick@momrik.com>
2017-01-12 19:51:23 -0500

    uptime: remove the decimal, which represents microseconds

--- home-assistant-pi.py
+++ home-assistant-pi.py
@@ -63,7 +63,7 @@ def get_uptime():
 	with open( '/proc/uptime', 'r' ) as f:
 		uptime_seconds = float( f.readline().split()[0] )
 
-	return str( timedelta( seconds = uptime_seconds ) )
+	return str( timedelta( seconds = uptime_seconds ) ).split(".")[0]
 
 # http://www.ridgesolutions.ie/index.php/2013/02/22/raspberry-pi-restart-shutdown-your-pi-from-python-code/
 def shutdown( restart = None ):
